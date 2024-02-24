0baec735e10634550b5486600e2f550f6d4381fc
abyalewt <99469291+abyalewt@users.noreply.github.com>
2024-02-25 02:52:30 +0300

    banner

--- netflix-clone/src/Pagess/Homes/Homes.js
+++ netflix-clone/src/Pagess/Homes/Homes.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Headers1 from "../../Comps/Headers1/Headers1";
 import Footerss from "../../Comps/Footerss/Footerss";
+import Banner from "../../Comps/Banner/Banner";
 
 const Homes = () => {
   return (
     <>
       <Headers1 />
+      <Banner />
       <Footerss />
     </>
   );
