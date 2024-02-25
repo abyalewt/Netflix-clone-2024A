cb79745fecae718d32cfc9315a31d3b3c38ce7fb
abyalewt <99469291+abyalewt@users.noreply.github.com>
2024-02-25 15:40:49 +0300

    row

--- netflix-clone/src/Pagess/Homes/Homes.js
+++ netflix-clone/src/Pagess/Homes/Homes.js
@@ -2,12 +2,14 @@ import React from "react";
 import Headers1 from "../../Comps/Headers1/Headers1";
 import Footerss from "../../Comps/Footerss/Footerss";
 import Banner from "../../Comps/Banner/Banner";
+import RowLists from "../../Comps/Rows/Rowlist/RowLists";
 
 const Homes = () => {
   return (
     <>
       <Headers1 />
       <Banner />
+      <RowLists />
       <Footerss />
     </>
   );
